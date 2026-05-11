dfacaf37fc5db2ec0be660577d85208e117d268a
priyanshu-pal88 <aachipal88@gmail.com>
2026-05-11 11:27:06 +0530

    setupes db

---
+++ backend/src/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+ const connectDB = async () => {
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+ }
+   
+
+ module.exports = connectDB;
